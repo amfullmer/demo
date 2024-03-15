c53153e505d2aa4d8e22e09002fd33327480b9fd
andie <155579891+amfullmer@users.noreply.github.com>
2024-03-14 20:34:27 -0400

    Add files via upload

---
+++ SecretScribe/src/Main/Main.java
@@ -0,0 +1,63 @@
+package Main;
+
+public class Main {
+    UserInterface userInterface = new UserInterface();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    public void run() {
+        int menuSelection = -1;
+
+        while (menuSelection != 0) {
+            userInterface.encodeOrDecode();
+            menuSelection = userInterface.promptForMenuSelection("Please choose an option: ");
+
+            //encoding messages
+            if (menuSelection == 1) {
+                userInterface.printCodeMessage();
+                userInterface.printLanguageOptions();
+                menuSelection = userInterface.promptForMenuSelection("Please choose and option: ");
+
+                if (menuSelection == 1) {
+                    userInterface.encodeMorseCode(userInterface.promptUserForMessage());
+                } else if (menuSelection == 2) {
+                    userInterface.encodeCaesarCipher(userInterface.promptUserForMessage());
+                } else if (menuSelection == 3) {
+                    userInterface.encodePigLatin(userInterface.promptUserForMessage());
+                } else {
+                    System.out.println("Invalid selection");
+                }
+
+                //decoding messages
+            } else if (menuSelection == 2) {
+                userInterface.printDecodeMessage();
+                userInterface.printLanguageOptions();
+                menuSelection = userInterface.promptForMenuSelection("Please choose and option: ");
+
+                if (menuSelection == 1) {
+                    userInterface.decodeMorseCode(userInterface.promptUserForMessage());
+                } else if (menuSelection == 2) {
+                    userInterface.decodeCaesarCipher(userInterface.promptUserForMessage());
+                } else if (menuSelection == 3) {
+                    userInterface.decodePigLatin(userInterface.promptUserForMessage());
+                } else {
+                    System.out.println("Invalid selection");
+                }
+
+            } else if (menuSelection == 0) {
+                continue;
+
+            } else {
+                // anything else is not valid
+                System.out.println("Invalid Selection");
+            }
+
+        }
+
+    }
+
+}
+
---
+++ SecretScribe/src/Test/CaesarCipherTest.java
@@ -0,0 +1,31 @@
+package Test;
+
+import Codes.CaesarCipher;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CaesarCipherTest {
+    @Test
+    public void encodeCaesarCipherTest_hello_world() {
+        String str = "Hello world!";
+        String expected = "Khoor zruog!";
+
+        CaesarCipher sut = new CaesarCipher();
+
+        String code = sut.encodeCaesarCipher(str);
+
+        Assert.assertEquals(expected, code);
+    }
+
+    @Test
+    public void decodeCaesarCipher_hello_world() {
+        String str = "KHOOR ZRUOG!";
+        String expected = "Hello world!";
+
+        CaesarCipher sut = new CaesarCipher();
+
+        String decode = sut.decodeCaesarCipher(str);
+
+        Assert.assertEquals(expected, decode);
+    }
+}
