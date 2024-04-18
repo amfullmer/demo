f605d9080d779b7721b9bfa0871adc63c3fc31d0
Andie Fullmer <amfullmer@gmail.com>
2024-04-18 12:00:18 -0400

    styles added

---
+++ secret-scribe/src/components/SecretMessage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="field">
+            <label class="label">Enter your message...</label>
+            <div class="control">
+                <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+            </div>
+        </div>
+        <div class="field">
+
+            <div class="control">
+                <div class="select">
+                    <select>
+                        <option>Select dropdown</option>
+                        <option>Caesar Cipher</option>
+                        <option>Morse Code</option>
+                        <option>Pig Latin</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="field is-grouped">
+            <div class="control">
+                <button class="button is-dark">Code</button>
+            </div>
+            <div class="control">
+                <button class="button is-dark">Decode</button>
+            </div>
+        </div>
+    </div>
+</template>
--- secret-scribe/src/views/HomeView.vue
+++ secret-scribe/src/views/HomeView.vue
@@ -1,3 +1,13 @@
 <template>
-  Welcome to Secret Scribe
+  <div>
+    Welcome to Secret Scribe
+    <br>
+    <router-link v-bind:to="{ name: 'secretMessage' }" class="button is-dark">Enter</router-link>
+  </div>
 </template>
+
+<script>
+
+export default {
+}
+</script>
