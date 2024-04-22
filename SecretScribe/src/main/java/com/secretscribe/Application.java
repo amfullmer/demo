fcd3da255ebce30896775028a41ba6788fcc057c
amfullmer <amfullmer@gmail.com>
2024-04-22 08:49:56 -0400

    security settings changed

--- SecretScribe/src/main/java/com/secretscribe/Application.java
+++ SecretScribe/src/main/java/com/secretscribe/Application.java
@@ -1,43 +1,13 @@
 package com.secretscribe;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class Application {
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//
-//}
-
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-//public class Application {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//
-//}
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    // Enable cross-origin requests for the entire application
-    @CrossOrigin
-    public class CrossOriginConfig {}
-
 }
