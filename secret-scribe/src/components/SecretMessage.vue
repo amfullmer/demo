5dd7f5dbf96a8a96d39c63d3e434661c865836f0
amfullmer <amfullmer@gmail.com>
2024-04-22 10:31:48 -0400

    MC encoded message coming through front end

--- secret-scribe/src/components/SecretMessage.vue
+++ secret-scribe/src/components/SecretMessage.vue
@@ -10,14 +10,13 @@
             {{ message }}
         </div>
         <div class="field">
-
             <div class="control">
                 <div class="select">
-                    <select>
+                    <select v-model="codeType">
                         <option>Select dropdown</option>
-                        <option>Caesar Cipher</option>
-                        <option>Morse Code</option>
-                        <option>Pig Latin</option>
+                        <option value="caesar">Caesar Cipher</option>
+                        <option value="morse">Morse Code</option>
+                        <!-- <option value="piglatin">Pig Latin</option> -->
                     </select>
                 </div>
             </div>
@@ -25,7 +24,7 @@
 
         <div class="field is-grouped">
             <div class="control">
-                <button class="button is-dark" @click="encodeCaesarCipher">Code</button>
+                <button class="button is-dark" @click="encodeMessage">Code</button>
             </div>
             <div class="control">
                 <button class="button is-dark">Decode</button>
@@ -40,21 +39,32 @@ import encodeService from '../services/EncodeService.js';
 export default {
     data() {
         return {
+            codeType: '',
             userText: '',
             message: ''
         }
     },
 
     methods: {
-        encodeCaesarCipher() {
-            encodeService.encodeCaesarCipher(this.userText)
-                .then(response => {
-                    this.message = response.data;
-                })
-                .catch(error => {
-                    console.error("Error encoding message:", error);
-                });
-        }
+        encodeMessage() {
+            if (this.codeType === 'caesar') {
+                encodeService.encodeCaesarCipher(this.userText)
+                    .then(response => {
+                        this.message = response.data;
+                    })
+                    .catch(error => {
+                        console.error("Error encoding message:", error);
+                    });
+            } else if (this.codeType === 'morse') {
+                encodeService.encodeMorseCode(this.userText)
+                    .then(response => {
+                        this.message = response.data;
+                    })
+                    .catch(error => {
+                        console.error("Error encoding message:", error);
+                    });
+            }
+        },
     }
 }
 </script>
